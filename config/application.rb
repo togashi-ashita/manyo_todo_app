8022e109b8a31c167de053c10f3e91712e870784
noriyuki togashi <>
2018-03-09 17:20:35 +0900

    ステップ10: Railsのタイムゾーンを設定しましょう

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module ManyoTodoApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
+    # 表示時のタイムゾーンをJSTに変更
+    config.time_zone = 'Tokyo'
+    # DB保存時のタイムゾーンをJSTに変更
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
